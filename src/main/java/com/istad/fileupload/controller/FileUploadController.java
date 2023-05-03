c7b5b3d3580f6d4841c7e7cac4e6aa07787b0b92
Sinaro1812 <1220.san.sinaro@rupp.edu.kh>
2023-05-03 11:07:25 +0700

    done

--- src/main/java/com/istad/fileupload/controller/FileUploadController.java
+++ src/main/java/com/istad/fileupload/controller/FileUploadController.java
@@ -1,14 +1,12 @@
 package com.istad.fileupload.controller;
 
+import com.istad.fileupload.Response.FileResponse;
 import com.istad.fileupload.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -16,20 +14,22 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/file")
 public class FileUploadController {
-    @Autowired
-    private FileService fileService;
+    private final FileService fileService;
+    FileUploadController(FileService fileService){
 
-    @Value("src/main/resources/images")
-    private String path;
-    @PostMapping("/upload") 
-    public ResponseEntity<com.istad.fileupload.Response.FileResponse> fileUpload(@RequestParam("image") MultipartFile image){
+        this.fileService = fileService;
+    }
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<FileResponse> fileUpload(@RequestParam("file") MultipartFile file){
         String filename = null;
         try {
-            filename = this.fileService.uploadImage(path, image);
+            filename = this.fileService.uploadFile(file );
 
         } catch (IOException e) {
-            return new ResponseEntity<>(new com.istad.fileupload.Response.FileResponse(null, "images can not upload !!!"), HttpStatus.OK);
+            return new ResponseEntity<>(new FileResponse(null, "images can not upload !!!"), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new com.istad.fileupload.Response.FileResponse(filename, "images is successfully upload to server!!"), HttpStatus.OK);
+        return new ResponseEntity<>(new FileResponse("http://localhost:8080"+filename, "images is successfully upload to server!!"), HttpStatus.OK);
     }
 }
--- src/main/java/com/istad/fileupload/service/FileService.java
+++ src/main/java/com/istad/fileupload/service/FileService.java
@@ -5,5 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 public interface FileService {
-    String uploadImage (String path, MultipartFile file) throws IOException;
+
+    String uploadFile(MultipartFile file) throws IOException;
 }
